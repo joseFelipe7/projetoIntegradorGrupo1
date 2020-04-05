f6c25f64cdf3cf4e70c46c7bc8215269aff72970
Luana Lencina <analulencina@hotmail.com>
2020-04-04 23:21:25 -0300

    Include cards notificações área usuário em desktop

--- app/routes/areaUsuarioRouter.js
+++ app/routes/areaUsuarioRouter.js
@@ -3,7 +3,9 @@ const express = require("express");
 let router = express.Router();
 
 router.get("/area-contratante", (req, res) => {
-    res.send("Área Usuário contratante");
+    // pagina para testar o include cards
+    res.render("areaUsuario");
+    // res.send("Área Usuário contratante");
 })
 
 router.get("/area-prestador", (req, res) => {
