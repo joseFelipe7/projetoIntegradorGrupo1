88e10e2b9f0a7f90a8ae106f536eb0ef84ae516a
jose felipe <josefelipedas.santos@gmail.com>
2020-04-05 18:07:16 -0300

    Menu Princiapal Desktop

--- app/routes/homeRouter.js
+++ app/routes/homeRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 let router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Home");
+    res.render("home");
 })
 
 router.get("/sobre", (req, res) => {
---
+++ public/js/script-menu.js
@@ -0,0 +1,4 @@
+function toggleMenuPrincipal(){
+    document.querySelector('#sub-menu-principal').classList.toggle('sub-menu-principal-enable')
+    document.querySelector('#menu-principal-itens-sub-click').classList.toggle('menu-principal-itens-sub-click')
+}
