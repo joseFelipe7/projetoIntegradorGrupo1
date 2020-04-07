0d69ec167becbfeadcd85b681a8a28706417c846
jose felipe <josefelipedas.santos@gmail.com>
2020-04-06 22:30:20 -0300

    menu de navegação mobile

--- public/js/script-menu.js
+++ public/js/script-menu.js
@@ -2,3 +2,8 @@ function toggleMenuPrincipal(){
     document.querySelector('#sub-menu-principal').classList.toggle('sub-menu-principal-enable')
     document.querySelector('#menu-principal-itens-sub-click').classList.toggle('menu-principal-itens-sub-click')
 }
+//mobile
+function abrirMenu(){
+    document.querySelector('.menu-principal-mobile').classList.toggle('menu-principal-mobile-fechar')
+    document.querySelector('.menu-principal-mobile').classList.toggle('menu-principal-mobile-abrir')
+}
