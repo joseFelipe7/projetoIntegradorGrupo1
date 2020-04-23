35529ca685858927453c00a3db8330eb22e6240e
jose felipe <josefelipedas.santos@gmail.com>
2020-04-22 23:30:07 -0300

    correção rotas

--- app/routes/prestadorRouter.js
+++ app/routes/prestadorRouter.js
@@ -10,17 +10,6 @@ router.get("/lista", (req, res) => {
     res.render("listaPrestadores");
 })
 
-router.get("/single/cadastro-servicos", (req, res) => {
-    res.render("areaPrestador", {view: "cadastroServiçosPrestador"});
-})
-
-router.get("/single/pedidos", (req, res) => {
-    res.render("areaPrestador", {view: "pedidosAreaPrestador"});
-})
-router.get("/single/meusDados-prestador-bru", (req, res) => {
-    res.render("areaPrestador", {view: "meusDados-prestador"});
-    // ...5620/prestador/single/meusDados-prestador-Bru
-})
 
 
 module.exports = router;
