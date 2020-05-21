63048b37cccad1d4099e3ce2ac9face66eaeeb8e
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-05-20 21:44:03 -0300

    Corrigir model prestador

--- models/Prestador.js
+++ models/Prestador.js
@@ -1,68 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
     const Prestador = sequelize.define("Prestador", {
-        email: {
-            type: DataTypes.STRING, 
-            allowNull: false
+        id_prestador: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement:true
         },
-        email2: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        },
-        password2: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        },
-        sobrenome: DataTypes.STRING,
-        sexo: DataTypes.STRING, 
-        data_nascimento: DataTypes.DATE,
-        rg:  DataTypes.STRING, 
-        emissor:  DataTypes.STRING, 
-        data_emissao:  DataTypes.DATE, 
-        telefone:  DataTypes.STRING, 
-        celular: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        },
-        email3: DataTypes.STRING,
-        endereco: DataTypes.STRING,
-        numero: DataTypes.STRING,
-        bairro: DataTypes.STRING,
-        cidade: DataTypes.STRING,
-        estado: DataTypes.STRING,
-        cep:  {
-            type: DataTypes.STRING, 
-            allowNull: false
-        },
-        cpf: {
-            type: DataTypes.STRING, 
-            allowNull: false
-        }, 
-        cnpj:  {
-            type: DataTypes.STRING, 
-            allowNull: false
-        },
-        nome_fantasia: DataTypes.STRING,
-        endereco2: DataTypes.STRING,
-        numero2: DataTypes.STRING,
-        bairro2: DataTypes.STRING,
-        cidade2: DataTypes.STRING,
-        estado2: DataTypes.STRING,
-        cep2: DataTypes.STRING,
-        telefone2: DataTypes.STRING,
-        celular2: DataTypes.STRING, 
-           
-        email4: DataTypes.STRING   
+        email: {type: Sequelize.STRING, allowNull: false},
+        password: {type: Sequelize.INTEGER, allowNull: false},
+        name: {type: Sequelize.STRING, allowNull: false},
+        data_nascimento: Sequelize.DATE,
+        cpf: {type: Sequelize.INTEGER, allowNull: false}, 
+        telefone: Sequelize.INTEGER,
+        celular: {type: Sequelize.INTEGER, allowNull: false}, 
+        avatar: Sequelize.STRING, 
+        createdAt: Sequelize.DATE, 
+        updatedAt: Sequelize.DATE, 
+        logradouro: Sequelize.STRING,
+        numero: Sequelize.INTEGER,
+        bairro: Sequelize.STRING,  
+        cidade: Sequelize.STRING,  
+        estado: Sequelize.STRING,  
+        cep: {type: Sequelize.INTEGER, allowNull: false},
+        cnpj: {type: Sequelize.INTEGER, allowNull: false},
+        nome_fantasia: Sequelize.STRING,
     }, {
-        tableName: 'prestador',
-        timestamps: false
+        tableName: 'prestador'
     });
     return Prestador;
 };
