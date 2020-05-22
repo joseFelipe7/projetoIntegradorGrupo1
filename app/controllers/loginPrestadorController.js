92eca4af5fad1b8a071ec3fd83897376eaf04481
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-05-22 16:11:15 -0300

    Autenticacao login prestador correcao

--- app/controllers/loginPrestadorController.js
+++ app/controllers/loginPrestadorController.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const config = require("../configs/database");
+const config = require("../../config/database");
 const bcrypt = require("bcrypt");
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
     const con = new Sequelize(config);
     
     const [user] = await con.query(
-      "SELECT * FROM users WHERE email=:email LIMIT 1",
+      "SELECT * FROM prestador WHERE email=:email LIMIT 1",
       {
         replacements: {
           email
@@ -23,7 +23,7 @@ module.exports = {
     );
 
     // Se não exister email ou password return mensagem de erro!!!
-    if (!user || !bcrypt.compareSync(password, umPrestador.password)) {
+    if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.render('loginPrestador', {
         msg: "Email ou password errados!!!"
       });
--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     username: 'root',
-    password: 'password',
-    database: 'toolsHall3',
+    password: '',
+    database: 'toolsHall',
     host:'127.0.0.1',
     dialect: 'mysql'
 }
