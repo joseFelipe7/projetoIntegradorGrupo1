0ec83262faa76ad4edc385afc416a02b0d05d4aa
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-05-22 19:54:52 -0300

    merge

--- config/database.js
+++ config/database.js
@@ -1,10 +1,6 @@
 module.exports = {
     username: 'root',
-<<<<<<< HEAD
-    password: 'password',
-=======
     password: '',
->>>>>>> 92eca4af5fad1b8a071ec3fd83897376eaf04481
     database: 'toolsHall',
     host:'127.0.0.1',
     dialect: 'mysql'
