d67d569253ba29e0934f7aaeafe428f8b6239919
brunaoflofl <bruna.oflima@gmail.com>
2020-05-22 23:11:21 -0300

    ultimas alterações

--- config/database.js
+++ config/database.js
@@ -1,10 +1,6 @@
 module.exports = {
     username: 'root',
-<<<<<<< HEAD
     password: 'password',
-=======
-    password: '',
->>>>>>> 6d25a03d067041a733cc8acf287697a00fc519a9
     database: 'toolsHall',
     host:'127.0.0.1',
     dialect: 'mysql'
