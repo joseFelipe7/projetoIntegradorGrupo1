d678a7194c1a07af83ac7fcd4c7fe2c4d7520964
Luana Lencina <analulencina@hotmail.com>
2020-05-27 21:08:29 -0300

    fix(header) - Altera header de usuário cliente loggado em todas as rotas da área contratante

--- app/routes/areaUsuarioRouter.js
+++ app/routes/areaUsuarioRouter.js
@@ -12,48 +12,48 @@ router.get("/area-contratante/meus-dados", authCliente, (req, res) => {
 
 
 router.get("/area-contratante/pedidos", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "pedidosAreaContratante"});
+    res.render("areaContratante", {view: "pedidosAreaContratante", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante/pedidos
 })
 
 
 router.get("/area-contratante/acompanhe", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "acompanheAreaContratante"});
+    res.render("areaContratante", {view: "acompanheAreaContratante", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante/acompanhe
 })
 
 
 router.get("/area-contratante/orcamentos-bru", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "orcamentoUsuario"});
+    res.render("areaContratante", {view: "orcamentoUsuario", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante/orcamento-bru
 })
 
 
 router.get("/area-contratante/historico-bru", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "historicoAreaContratante"});
+    res.render("areaContratante", {view: "historicoAreaContratante", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante-bru/historico
 })
 
 
 router.get("/area-contratante/chat", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "chat"});
+    res.render("areaContratante", {view: "chat", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante/chat
 })
 
 
 router.get("/area-contratante/favoritos", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "favoritosAreaContratante"});
+    res.render("areaContratante", {view: "favoritosAreaContratante", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante/favoritos
 })
 
 
 router.get("/area-contratante/cupons", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "cuponsAreaContratante"});
+    res.render("areaContratante", {view: "cuponsAreaContratante", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante/cupons
 })
 
 router.get("/area-contratante/pagamentos-bru", authCliente, (req, res) => {
-    res.render("areaContratante", {view: "pagamentos"});
+    res.render("areaContratante", {view: "pagamentos", loggado: req.session.cliente});
     // ...5620/usuario/area-contratante/pagamentos-bru
 })
 
