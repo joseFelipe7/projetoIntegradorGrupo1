07d081c688a7e4c1d05232b83da218d1e88be761
Luana Lencina <analulencina@hotmail.com>
2020-06-03 22:44:00 -0300

    feat(chat) - Adiciona relação belongsTo Adiciona relação belongsTo entre Chat e fk prestador e cliente

--- app/models/Chat.js
+++ app/models/Chat.js
@@ -25,15 +25,35 @@ module.exports = (sequelize, DataTypes) => {
     hasMany // tem muitos
     */
 
-    //defindo a relaçao com a tabela mensagens, 1:n
+    //defindo a relaçao com a tabela mensagem, 1:n
+    //Chat possuiMuitas mensagens
     Chat.associate = (listaModels)=>{
-        Chat.hasMany(listaModels.Mensagens,{
+        Chat.hasMany(listaModels.Mensagem,{
             forenKey:'id_chats',
             //fk da tabela em que está sendo o relacionamento, o id dessa tabela na outra (fk_chats)
-            //procura a fk dentro da model que tenho relação, no caso: na Mensagens
+            //procura a fk dentro da model que tenho relação, no caso: na Mensagem
             as:'mensagens',
             //alias, apelido para a fk da relaçao
-        })
+        });
+
+        //defindo a relaçao com fk do prestador na tabela chat
+        //Chat pertenceAum prestador
+        Chat.belongsTo(listaModels.Prestador,{
+            forenKey:'prestadores_id',
+            //nome da fk dentro da tabela que carrega a relação: Chat
+            as:'prestador',
+            //alias, apelido para a fk da relaçao
+        });
+
+        //defindo a relaçao com fk do cliente na tabela chat
+        //Chat pertenceAum cliente
+        Chat.belongsTo(listaModels.Cliente,{
+            forenKey:'clientes_id',
+            //nome da fk dentro da tabela que carrega a relação: Chat
+            as:'cliente',
+            //alias, apelido para a fk da relaçao
+        });
     }
+
     return Chat;
 };
