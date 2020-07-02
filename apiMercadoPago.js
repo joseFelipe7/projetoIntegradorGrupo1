da47a69c557d8c175a1d69c62c97fe1448038786
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-07-01 23:02:39 -0300

    Tirar arquivo com dados da api

--- apiMercadoPago.js
+++
@@ -1,9 +0,0 @@
-const email = 'toolshallservicos@gmail.com';
-const senha = 'toolshall2020';
-
-const publicKey = 'TEST-a1f2d7c5-e677-42d0-82e9-e53c31d2eb8c';
-const accessToken = 'TEST-5168209495579913-062613-22ba103c85a881d770b4b41dd0b874ae-593069833';
-
-const clientId = 5168209495579913;
-const clientSecret = '3SMxaQdp1ZBXPvDOEhFl2uRf7NleoMMY';
-
