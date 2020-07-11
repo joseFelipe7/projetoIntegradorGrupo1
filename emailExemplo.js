f7120d4083c5ea4e452ec4dda15db31ffa96b790
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-07-11 09:28:04 -0300

    Corrigir arquivo exemplo email

--- emailExemplo.js
+++ emailExemplo.js
@@ -1,6 +1,7 @@
 //configuração de email para usar em newsletter e no esqueci a senha.
 const nodemailer = require("nodemailer");
 
+// Substituir "emailExemplo" por somente "email".
 const emailExemplo = nodemailer.createTransport({
     service: 'informe serviço de email',
     auth:{
